2ad2bc577852c71c03ffa2791d3b36dda007159a
andriidem <andriidem308@gmail.com>
2021-03-16 11:41:50 +0200

    try number 3

--- parse_cookie.py
+++ parse_cookie.py
@@ -1,10 +1,10 @@
 def parse_cookie(query: str) -> dict:
     dct = {}
     if query:
-        options = query.split(';')
-        for o in options:
+        for o in query.split(';'):
             if '=' in o:
-                dct[o.split('=')[0]] = '='.join(o.split('=')[1:])
+                o = o.split('=')
+                dct[o[0]] = '='.join(o[1:])
 
     return dct
 
@@ -14,3 +14,4 @@ if __name__ == '__main__':
     assert parse_cookie('') == {}
     assert parse_cookie('name=Dima;age=28;') == {'name': 'Dima', 'age': '28'}
     assert parse_cookie('name=Dima=User;age=28;') == {'name': 'Dima=User', 'age': '28'}
+
