de8653dd32514f53ca27cdad856c3048e135f330
andriidem <andriidem308@gmail.com>
2021-03-16 10:10:06 +0200

    commited parse_cookie

---
+++ parse_cookie.py
@@ -0,0 +1,9 @@
+def parse_cookie(query: str) -> dict:
+    return {}
+
+
+if __name__ == '__main__':
+    assert parse_cookie('name=Dima;') == {'name': 'Dima'}
+    assert parse_cookie('') == {}
+    assert parse_cookie('name=Dima;age=28;') == {'name': 'Dima', 'age': '28'}
+    assert parse_cookie('name=Dima=User;age=28;') == {'name': 'Dima=User', 'age': '28'}
