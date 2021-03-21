7b4eac56ea482cd3701c9ac790df4ef6b2296f32
andriidem308 <andriidem308@gmail.com>
2021-03-21 21:54:48 +0200

    updated

--- flask_tasks/flask_01/__init__.py
+++ flask_tasks/flask_01/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 from faker import Faker
 from string import printable
 from random import choice
@@ -10,41 +10,36 @@ app = Flask(__name__)
 
 @app.route('/')
 def main_page() -> str:
-    res = "<h1><a href='/'>Main Page</a></h1>"
-    res += "<a href='/users/generate'>Generate users</a><br>"
-    res += "<a href='/password/generate'>Generate passwords</a><br>"
-    res += "<a href='/astro'>Astronauts' amount at the moment</a><br>"
-    return res
+    return render_template('index.html')
 
 
 @app.route('/users/generate')
 @app.route('/users/generate/<int:n>')
 def generate_users(n=20) -> str:
-    res = "<h1><a href='/'>Main Page</a></h1>"
-    gen_names = [Faker().name() for _ in range(n)]
-    gen_emails = [Faker().free_email() for _ in range(n)]
-    res += '<br>'.join(map(', '.join, tuple(zip(gen_names, gen_emails))))
-    return res
+    fk = Faker()
+    gen_names = [fk.name() for _ in range(n)]
+    gen_emails = [fk.free_email() for _ in range(n)]
+    gen_users = tuple(zip(gen_names, gen_emails))
+    res = '<br>' + '<br>'.join(map(', '.join, gen_users))
+    return render_template('users-gen.html') + res
 
 
 @app.route('/password/generate')
 @app.route('/password/generate/<int:n>')
 def generate_password(n=8) -> str:
-    res = "<h1><a href='/'>Main Page</a></h1>"
     password = ''
     for _ in range(n):
         password += choice(printable)
 
-    res += 'Random password: ' + password
-    return res
+    res = f'<p>Random password: {password}</p>'
+    return render_template('pass-gen.html') + res
 
 
 @app.route('/astro')
 def astro_counter() -> str:
-    res = "<h1><a href='/'>Main Page</a></h1>"
     json_data = requests.get("http://api.open-notify.org/astros.json", json={"key": "value"}).json()
-    res += "Astronauts' amount: " + str(json_data['number'])
-    return res
+    res = "Astronauts' amount: " + str(json_data['number'])
+    return render_template('astro.html') + res
 
 
 if __name__ == "__main__":
